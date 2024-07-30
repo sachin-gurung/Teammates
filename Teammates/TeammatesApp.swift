c439467e9f67991fd85d79d494a825f840175036
sachin-gurung <saching061@hotmail.com>
2024-07-30 13:25:35 -0700

    Initial Commit

---
+++ Teammates/TeammatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeammatesApp.swift
+//  Teammates
+//
+//  Created by Sachin Gurung on 7/30/24.
+//
+
+import SwiftUI
+
+@main
+struct TeammatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

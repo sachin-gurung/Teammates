d29f6a86b0ff0bfd2890dd7b75a2857533bcd6e6
sachin-gurung <saching061@hotmail.com>
2024-07-30 14:49:16 -0700

    Created the bottom navigation bar with 4 buttons

--- Teammates/ContentView.swift
+++ Teammates/ContentView.swift
@@ -9,13 +9,70 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Spacer()
+        
+       // Bottom navigation bar
+        HStack{
+            VStack{
+                Button(action: {
+                    //TODO: Action
+                }) {
+                    Image(systemName: "house.fill")
+                        .font(.system(size: 24))
+                        .padding()
+                }
+                Text("Home")
+                    .font(.caption)
+                    .foregroundColor(.black)
+            }
+            
+            Spacer()
+            
+            VStack{
+                Button(action: {
+                    //TODO: Action
+                }) {
+                    Image(systemName: "square.grid.3x3.fill")
+                        .font(.system(size: 24))
+                        .padding()
+                }
+                Text("Tools")
+                    .font(.caption)
+                    .foregroundColor(.black)
+            }
+            
+            Spacer()
+            
+            VStack{
+                Button(action: {
+                    //TODO: Action
+                }) {
+                    Image(systemName: "message.fill")
+                        .font(.system(size: 24))
+                        .padding()
+                }
+                Text("Messages")
+                    .font(.caption)
+                    .foregroundColor(.black)
+            }
+            
+            Spacer()
+            
+            VStack{
+                Button(action: {
+                    //TODO: Action
+                }) {
+                    Image(systemName: "gearshape.fill")
+                        .font(.system(size: 24))
+                        .padding()
+                }
+                Text("Settings")
+                    .font(.caption)
+                    .foregroundColor(.black)
+            }
         }
         .padding()
+        .background(Color(UIColor.systemGray6))
     }
 }
 

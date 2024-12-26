9730fa278be40d0512dc796e7fb4a6d530ab03c7
sachin-gurung <saching061@hotmail.com>
2024-12-25 21:24:10 -0800

    Added new target for Seattle Nepali School App

---
+++ SeattleNepaliSchool/SeattleNepaliSchoolApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeattleNepaliSchoolApp.swift
+//  SeattleNepaliSchool
+//
+//  Created by Sachin Gurung on 12/25/24.
+//
+
+import SwiftUI
+
+@main
+struct SeattleNepaliSchoolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
